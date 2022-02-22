0d59cbd4f5364ea11004194d98b86812e1d19682
szuwgh <895340293@qq.com>
2022-02-22 16:53:41 +0800

    save

--- src/decoder.rs
+++ src/decoder.rs
@@ -22,4 +22,24 @@ impl Decoder {
     fn find_target_arc(_in: u8) -> Option<Arc> {
         None
     }
+
+    fn read_v_u64() {
+        
+        byte b = readByte();
+    if (b >= 0) return b;
+    int i = b & 0x7F;
+    b = readByte();
+    i |= (b & 0x7F) << 7;
+    if (b >= 0) return i;
+    b = readByte();
+    i |= (b & 0x7F) << 14;
+    if (b >= 0) return i;
+    b = readByte();
+    i |= (b & 0x7F) << 21;
+    if (b >= 0) return i;
+    b = readByte();
+    // Warning: the next ands use 0x0F / 0xF0 - beware copy/paste errors:
+    i |= (b & 0x0F) << 28;
+    if ((b & 0xF0) == 0) return i;
+    }
 }
